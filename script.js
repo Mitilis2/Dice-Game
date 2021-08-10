399aabd657412e7b88c528532a05330e70352f28
Mitul Shah <mitulshah91@gmail.com>
2021-08-10 19:41:20 -0400

    continue dice roll logic

--- script.js
+++ script.js
@@ -6,26 +6,37 @@ const playerOneScore = document.querySelector('.player-1-score');
 const playerTwoScore = document.querySelector('.player-2-score');
 const playerCardOne = document.querySelectorAll('.player-card')[0];
 const playerCardTwo = document.querySelector('.player-card-2')
-
-
-
 const diceImg1 = document.querySelector('.dice-img-1');
 const diceImg2 = document.querySelector('.dice-img-2');
+
+
+
+
 let currentScore1 = 0;
 let currentScore2 = 0;
 let playing = true;
 // initalize game
 
-function initalizeGame(){
+
+const initial = function (){
+    playing = true;
     playerOneScore.innerHTML = 0;
     playerTwoScore.innerHTML = 0;
-    // hide winner message and modals
+    currentScore1 = 0;
+    currentScore2 = 0;
+        // hide winner message and modals
     // add modal for player name 1 & player name 2;
     diceImg1.src = "/img/dice-1.png"
     diceImg2.src = "/img/dice-2.png"
 }
-initalizeGame();
+initial();
 
+function playerOneWins(){
+
+}
+function playerTwoWins(){
+
+}
 // roll button functionality
 
 
@@ -35,25 +46,42 @@ rollBtn.addEventListener('click', function(){
     diceImg1.src = `/img/dice-${dice}.png` 
     const dice2 = Math.trunc(Math.random() * 6) + 1
     diceImg2.src = `/img/dice-${dice2}.png`;
-    
     currentScore1 += dice;
     currentScore2 += dice2;
     playerOneScore.innerHTML = currentScore1;
     playerTwoScore.innerHTML = currentScore2;
 }
-// game logic
-
-});
-
-
-
-
-
+// end the game, no more dice rolls when a player reaches the  set score (default =  10)
+    if(playerOneScore.innerHTML >= 10){
+        playing = false;
+        console.log('Player 1 wins!')
+    } else if(playerTwoScore.innerHTML >= 10){
+        playing = false;
+        console.log('Player 2 wins!')
+    }
+    else{
+        console.log('neither player reached the target score')
+    }
+    if(playerOneScore.innerHTML >= 10 && playerTwoScore.innerHTML < 10){
+            playing =  false;
+            console.log('Player 1 beat Player 2')
+        } else if(playerTwoScore.innerHTML >= 10 && playerOneScore.innerHTML < 10) {
+            playing =  false;
+            console.log('Player 2 beat Player 1')
+        } else if(playerOneScore.innerHTML >= 10 && playerTwoScore.innerHTML >= 10){
+           if(playerOneScore.innerHTML > playerTwoScore.innerHTML){
+               console.log('Player 1 has higher score')
+           } else if(playerTwoScore.innerHTML > playerOneScore.innerHTML){
+            console.log('Player 2 has higher score')
+           }else {
+                console.log('No winner')
+            }
+           }
+        });
 
 
 // reset button functionality
-
 resetBtn.addEventListener('click', function(){
- initalizeGame()
+ initial()
 });
 
