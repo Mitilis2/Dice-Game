c5b02cfbbafc5d834c95696731465bdd10cc1fc0
Mitul Shah <mitulshah91@gmail.com>
2021-08-10 21:12:23 -0400

    remove imgs folder, change html/js

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ const initial = function (){
     currentScore2 = 0;
         // hide winner message and modals
     // add modal for player name 1 & player name 2;
-    diceImg1.src = "/img/dice-1.png"
-    diceImg2.src = "/img/dice-2.png"
+    diceImg1.src = "/dice-1.png"
+    diceImg2.src = "/dice-2.png"
 }
 initial();
 
@@ -43,9 +43,9 @@ function playerTwoWins(){
 rollBtn.addEventListener('click', function(){
     if(playing){
     const dice = Math.trunc(Math.random() * 6) + 1
-    diceImg1.src = `/img/dice-${dice}.png` 
+    diceImg1.src = `/dice-${dice}.png` 
     const dice2 = Math.trunc(Math.random() * 6) + 1
-    diceImg2.src = `/img/dice-${dice2}.png`;
+    diceImg2.src = `/dice-${dice2}.png`;
     currentScore1 += dice;
     currentScore2 += dice2;
     playerOneScore.innerHTML = currentScore1;
